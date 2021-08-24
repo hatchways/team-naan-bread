1728b7c53123dcd9717bd9ea3e3eadd897c2ea66
mohamedelserafy <elserafy02@gmail.com>
2021-08-25 00:10:14 +0200

    update   -when a user is logged in the app will redirect to the dashboard only after logging in or registration

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -25,7 +25,9 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   const updateLoginContext = useCallback(
     (data: AuthApiDataSuccess) => {
       setLoggedInUser(data.user);
-      history.push('/dashboard');
+      if (history.location.pathname === '/login' || history.location.pathname === '/register') {
+        history.push('/dashboard');
+      }
     },
     [history],
   );
@@ -46,7 +48,10 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
-          history.push('/dashboard');
+          console.log(history);
+          if (history.location.pathname === '/login' || history.location.pathname === '/register') {
+            history.push('/dashboard');
+          }
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
