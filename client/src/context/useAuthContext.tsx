1a81a06e7618c73a1138280f27bfa582b75bd91e
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-08-26 12:59:19 -0400

    prevented it from constantly pushing to dashboard

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -21,11 +21,13 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   // default undefined before loading, once loaded provide user or null if logged out
   const [loggedInUser, setLoggedInUser] = useState<User | null | undefined>();
   const history = useHistory();
-
   const updateLoginContext = useCallback(
     (data: AuthApiDataSuccess) => {
       setLoggedInUser(data.user);
-      history.push('/dashboard');
+      // history.push('/dashboard');
+      if (history.location.pathname === '/login' || history.location.pathname === '/register') {
+        history.push('/dashboard');
+      }
     },
     [history],
   );
@@ -46,7 +48,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       await loginWithCookies().then((data: AuthApiData) => {
         if (data.success) {
           updateLoginContext(data.success);
-          history.push('/dashboard');
+          // history.push('/dashboard');
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
