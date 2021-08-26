b95d232d1660315bf8740a6823baf731d822c745
Leo Blanco <leopoldoblanco@gmail.com>
2021-08-26 09:59:07 -0300

    updated PUT route

--- server/routes/request.js
+++ server/routes/request.js
@@ -5,5 +5,6 @@ const protect = require('../middleware/auth');
 
 router.route('/').get(getRequests);
 router.route('/').post(postRequest);
+router.route('/:id').put(postRequest);
 
 module.exports = router;
