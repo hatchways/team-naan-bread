8816b88ae5aa964ba4f2b9ce935842b8c9bc8f39
Leo Blanco <leopoldoblanco@gmail.com>
2021-08-29 23:13:37 -0300

    fetching requests

--- client/src/pages/MySitters/MySitters.tsx
+++ client/src/pages/MySitters/MySitters.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
+import { useState, useEffect } from "react";
+import getRequests from '../../helpers/APICalls/getRequests';
+import { RequestData } from "../../interface/Request";
+import { Container, Grid, Box } from '@material-ui/core';
 import useStyles from "./useStyles";
 
+export default function MySitters(): JSX.Element {
+  const [requests, setRequests] = useState<RequestData[]>([]);
 
-const MySitters = () => {
+  useEffect(() => {
+    getRequests()
+      .then((data) => setRequests([data]));
+  }, []);
+
+  console.log(requests)
+
   return (
-    <div>
-      My Sitters Page
-    </div>
+    <Container>
+      <Grid container alignItems="center">
+        <Grid item xs={12}>
+          My Sitters Page
+        </Grid>
+        <Box>
+          <Grid item xs={12} sm={5}>
+            Requests listings
+          </Grid>
+          <Grid item xs={12} sm={7}>
+            Calendar
+          </Grid>
+        </Box>
+      </Grid>
+    </Container>
   )
 };
-
-export default MySitters;
