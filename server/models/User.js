91b705ce4b9c022e9ef6ef83b3b8b3d51657d3cc
Leo Blanco <leopoldoblanco@gmail.com>
2021-08-31 11:39:19 -0300

    update user and request models

--- server/models/User.js
+++ server/models/User.js
@@ -20,6 +20,14 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  firstName: {
+    type: String,
+    default: "",
+  },
+  lastName: {
+    type: String,
+    default: "",
+  },
   profilePhoto: {
     url: String,
     publicId: String,
