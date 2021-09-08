450ca291ea095a1955dc2a8b6058cd9f0712b029
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-09-08 15:58:10 -0400

    added profilePic to profile model

--- client/src/pages/Profile/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/pages/Profile/ProfilePhoto/ProfilePhoto.tsx
@@ -9,6 +9,9 @@ import uploadProfilePhoto from '../../../helpers/APICalls/uploadProfilePhoto';
 import { User } from '../../../interface/User';
 import deleteProfilePhoto from '../../../helpers/APICalls/deleteProfilePhoto';
 import { CircularProgress } from '@material-ui/core';
+import getProfile from '../../../helpers/APICalls/getProfile';
+import { ProfileApiData } from '../../../interface/ProfileApiData';
+
 interface Props {
   loggedInUser: User;
 }
@@ -29,8 +32,12 @@ export default function ProfilePhoto({ loggedInUser }: Props): JSX.Element {
   });
 
   useEffect(() => {
-    setPhoto(loggedInUser.profilePhotoUrl);
-  }, [loggedInUser.profilePhotoUrl]);
+    if (loggedInUser) {
+      getProfile(loggedInUser.id).then((data: ProfileApiData) => {
+        setPhoto(data.profilePhoto?.url);
+      });
+    }
+  }, [loggedInUser]);
 
   const changePhoto = (file: File | null) => {
     setLoading(true);
