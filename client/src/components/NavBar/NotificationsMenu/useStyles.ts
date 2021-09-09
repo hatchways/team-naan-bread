84aa285d685cf82f8e35dc9b7dc6f6467578f2e8
mohamedelserafy <elserafy02@gmail.com>
2021-09-09 21:26:17 +0200

    fix
        - added the notification avatar style back

--- client/src/components/NavBar/NotificationsMenu/useStyles.ts
+++ client/src/components/NavBar/NotificationsMenu/useStyles.ts
@@ -1,13 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
-  notification: {
-    fontSize: '12px',
-    fontWeight: 600,
-    color: '#000000',
-  },
-  notificationType: {
-    fontSize: '10px',
+  notificationAvatar: {
+    marginRight: '14px',
+    width: '55px',
+    height: '55px',
   },
 }));
 export default useStyles;
