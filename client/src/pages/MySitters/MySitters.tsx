7bec94c35d4aa4f3844ee5c5b793462aacd745d3
mohamedelserafy <elserafy02@gmail.com>
2021-09-11 17:42:48 +0200

    grow animation to my-sitters

--- client/src/pages/MySitters/MySitters.tsx
+++ client/src/pages/MySitters/MySitters.tsx
@@ -2,15 +2,15 @@
 import React, { useState, useEffect } from 'react';
 import { RequestData } from "../../interface/Request";
 import getRequests from '../../helpers/APICalls/getRequests';
-import { 
-  Container, 
-  Grid, 
-  Box, 
-  Card, 
-  CardHeader, 
-  CardContent, 
+import {
+  Container,
+  Grid,
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
   IconButton,
-  Typography 
+  Typography, Grow,
 } from '@material-ui/core';
 import { Settings } from "@material-ui/icons";
 import useStyles from "./useStyles";
@@ -23,30 +23,29 @@ export default function MySitters(): JSX.Element {
   const classes = useStyles();
 
   const [requests, setRequests] = useState<RequestData[]>([]);
-  const [dates, setDates] = useState<Date[]>([])
+  const [dates, setDates] = useState<Date[]>([]);
 
   useEffect(() => {
-    getRequests()
-      .then(data => {
-        setRequests(data)
-        const dates: Date[] = [];
-        data.map(booking => {
-          if (booking.accepted) {
-            const bookingDate = new Date(booking.start)
-            dates.push(bookingDate)
-          }
-        });
-        setDates(dates);
+    getRequests().then((data) => {
+      setRequests(data);
+      const dates: Date[] = [];
+      data.map((booking) => {
+        if (booking.accepted) {
+          const bookingDate = new Date(booking.start);
+          dates.push(bookingDate);
+        }
+      });
+      setDates(dates);
     });
   }, []);
-  
+
   const currentDate = new Date();
 
   const nextBookingFinder = (bookings: RequestData[]) => {
-    const nextBooking: RequestData[] = []
+    const nextBooking: RequestData[] = [];
     bookings.forEach((booking) => {
       if (booking.accepted && !nextBooking.length) {
-        nextBooking.push(booking)
+        nextBooking.push(booking);
       }
     });
     return nextBooking;
@@ -55,84 +54,81 @@ export default function MySitters(): JSX.Element {
   const currentBookings = requests.filter((request) => {
     const requestDate = new Date(request.start);
     return requestDate >= currentDate;
-  });  
-  
+  });
+
   const nextBooking: RequestData[] = nextBookingFinder(currentBookings);
 
   const pastBookings = requests.filter((request) => {
     const requestDate = new Date(request.start);
     return requestDate < currentDate;
-  })
+  });
 
   // function to display bookings dates
   const createDate = (dateStart: any, dateEnd: any) => {
     dateStart = new Date(dateStart);
     dateEnd = new Date(dateEnd);
     const day = dateStart.getDate();
-    let month = dateStart.toLocaleDateString("us-US", {month: "long"});
+    let month = dateStart.toLocaleDateString('us-US', { month: 'long' });
     month = month.charAt(0).toUpperCase() + month.slice(1);
     const year = dateStart.getFullYear();
     const timeStart = dateStart.getHours();
     const timeEnd = dateEnd.getHours();
-    return `${day}, ${month} ${year}, ${timeStart}-${timeEnd} HS.`
-  }
+    return `${day}, ${month} ${year}, ${timeStart}-${timeEnd} HS.`;
+  };
 
   return (
     <Container className={classes.root}>
       <Box className={classes.mySittersContent} component="main">
-        <Grid item xs={12} sm={5} className={classes.mySittersColumns}>
-          <Box className={classes.cardWrapperNext}>
-            <Card variant="outlined">
-              <CardHeader 
-                classes={{title: classes.cardBookingHeaderTitle}}
-                title="Your next booking" 
-                action={
-                  <IconButton aria-label="settings">
-                    <Settings color="disabled" />
-                  </IconButton>
-                }
-                className={classes.cardBookingHeader} 
-              />
-              <CardContent className={classes.cardBookingContent}>
-                <Typography variant="h6" component="h6" className={classes.cardDate}>
-                  {currentBookings.length 
-                  && createDate(nextBooking[0]?.start, nextBooking[0]?.end)}
-                </Typography>
-                <Box className={classes.cardUserInfoBox} >
-                  <Box>
-                    <img src={mockImg} alt="user" className={classes.cardUserThumbnail}/>
-                  </Box>
-                  <Typography component="h3" className={classes.cardUserName}>
-                    {`
+        <Grow in={true}>
+          <Grid item xs={12} sm={5} className={classes.mySittersColumns}>
+            <Box className={classes.cardWrapperNext}>
+              <Card variant="outlined">
+                <CardHeader
+                  classes={{ title: classes.cardBookingHeaderTitle }}
+                  title="Your next booking"
+                  action={
+                    <IconButton aria-label="settings">
+                      <Settings color="disabled" />
+                    </IconButton>
+                  }
+                  className={classes.cardBookingHeader}
+                />
+                <CardContent className={classes.cardBookingContent}>
+                  <Typography variant="h6" component="h6" className={classes.cardDate}>
+                    {currentBookings.length && createDate(nextBooking[0]?.start, nextBooking[0]?.end)}
+                  </Typography>
+                  <Box className={classes.cardUserInfoBox}>
+                    <Box>
+                      <img src={mockImg} alt="user" className={classes.cardUserThumbnail} />
+                    </Box>
+                    <Typography component="h3" className={classes.cardUserName}>
+                      {`
                       ${nextBooking[0]?.user.firstName} 
                       ${nextBooking[0]?.user.lastName} 
                     `}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </Box>
-          <Box className={classes.cardWrapperBookings}>
-            <CardHeader
-              title="Current Bookings"
-              classes={{title: classes.cardBookingHeaderTitle}}
-            />
-            <BookingCard createDate={createDate} requests={currentBookings} />
-          </Box>
-          <Box className={classes.cardWrapperBookings}>
-            <CardHeader
-              title="Past Bookings"
-              classes={{title: classes.cardBookingHeaderTitle}}
-            />
-            <Box overflow="hidden">
-              <BookingCard createDate={createDate}  requests={pastBookings}/>
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Card>
             </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={7} className={classes.mySittersColumns}>
-          <BookingsDatePicker dates={dates} />
-        </Grid>
+            <Box className={classes.cardWrapperBookings}>
+              <CardHeader title="Current Bookings" classes={{ title: classes.cardBookingHeaderTitle }} />
+              <BookingCard createDate={createDate} requests={currentBookings} />
+            </Box>
+            <Box className={classes.cardWrapperBookings}>
+              <CardHeader title="Past Bookings" classes={{ title: classes.cardBookingHeaderTitle }} />
+              <Box overflow="hidden">
+                <BookingCard createDate={createDate} requests={pastBookings} />
+              </Box>
+            </Box>
+          </Grid>
+        </Grow>
+        <Grow in={true}>
+          <Grid item xs={12} sm={7} className={classes.mySittersColumns}>
+            <BookingsDatePicker dates={dates} />
+          </Grid>
+        </Grow>
       </Box>
     </Container>
-  )
-};
+  );
+}
