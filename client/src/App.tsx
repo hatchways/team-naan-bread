7b5143270076d88d13e69ac05b0ba667dedf754c
mohamedelserafy <elserafy02@gmail.com>
2021-09-14 09:43:31 +0200

    moved reactor steps to helpers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,20 +14,7 @@ import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
 import { TourProvider } from '@reactour/tour';
-const steps = [
-  {
-    selector: '[data-tour="step-1"]',
-    content: 'you can see your booking from here',
-  },
-  {
-    selector: '[data-tour="step-2"]',
-    content: 'you can click here to edit your profile',
-  },
-  {
-    selector: '[data-tour="step-3"]',
-    content: 'you can click here to add a picture of your self',
-  },
-];
+import { steps } from './helpers/Reactour/reactorSteps';
 
 import './App.css';
 
