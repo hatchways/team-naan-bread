96d78460fac0bd6acc567f3c7ae7a61bdcabe654
mohamedelserafy <elserafy02@gmail.com>
2021-09-23 15:28:23 +0200

    edit event

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -8,3 +8,19 @@ exports.createEvent = asyncHandler(async (req, res, next) => {
 
   return res.status(201).json(newEvent);
 });
+
+exports.editEvent = asyncHandler(async (req, res, next) => {
+  const { name, eventDate, location } = req.body;
+  const hostId = req.user.id;
+  const eventId = req.query.id;
+  const petEvent = await Event.findById(eventId);
+  if (!petEvent) {
+    return res.sendStatus(404);
+  }
+  if (!petEvent.host.equals(hostId)) {
+    return res.sendStatus(403);
+  }
+  const editedEvent = await Event.findOneAndUpdate({ _id: eventId }, { name, eventDate, location }, { new: true });
+
+  return res.status(201).json(editedEvent);
+});
--- server/routes/event.js
+++ server/routes/event.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const protect = require('../middleware/auth');
 
-const { createEvent } = require('../controllers/event');
+const { createEvent, editEvent } = require('../controllers/event');
 
 router.route('/').post(protect, createEvent);
+router.route('/').put(protect, editEvent);
 
 module.exports = router;
