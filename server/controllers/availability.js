675e0f1c2e2171ff640c05536b900b043a20d5a8
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-09-23 17:15:49 -0400

    formatting changes

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -1,47 +1,47 @@
 const Availability = require("../models/Availability");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 //the Profile is also created on in controllers/auth when the user creates a new account. However- only the email and id fields will be filled
 
+exports.updateAvailability = asyncHandler(async (req, res) => {
+  for (let prop in req.body.values.availability) {
+    if (!req.body.values.availability[prop]) {
+      delete req.body.values.availability[prop];
+      //it will remove fields who are undefined or null
+    }
+  }
 
-  exports.updateAvailability = asyncHandler(async (req, res) => {
+  availability = await Availability.findOneAndUpdate(
+    { _id: mongoose.Types.ObjectId(req.body.values._id) },
+    { availability: req.body.values.availability },
+    { new: true, strict: false }
+  );
 
-     for(let prop in req.body.values.availability){
-      if(!req.body.values.availability[prop]){
-        delete req.body.values.availability[prop];
-        //it will remove fields who are undefined or null
-      }
-     }
+  //if there is no availability for whatever reason- create it
+  if (!availability) {
+    availability = await Availability.create({
+      _id: new mongoose.Types.ObjectId(req.body.values._id),
+      availability: req.body.values.availability,
+    });
+  }
 
-     availability = await Availability.findOneAndUpdate(
-    {_id: mongoose.Types.ObjectId(req.body.values._id)},
-    {availability: req.body.values.availability},
-    {new: true, strict: false});
+  res.status(200).json(availability);
+});
+exports.findAvailabilityById = asyncHandler(async (req, res) => {
+  const availability = await Availability.findById(req.body.id);
 
-     //if there is no availability for whatever reason- create it
-     if(!availability) {
-      availability = await Availability.create({_id: new mongoose.Types.ObjectId(req.body.values._id), availability: req.body.values.availability});
-     }
+  if (!availability) {
+    res.status(404).send("No availability found for ID");
+  }
+  res.status(200).json(availability.availability);
+});
 
-     res.status(200).json(availability);
-
-})
-  exports.findAvailabilityById = asyncHandler(async (req, res) => {
-
-     const availability = await Availability.findById(req.body.id);
-
-     if (!availability) {
-        res.status(404).send("No availability found for ID");
-      }
-    res.status(200).json(availability.availability);
-})
-
-  exports.findAllAvailability = asyncHandler(async (req, res) => {
+exports.findAllAvailability = asyncHandler(async (req, res) => {
   const availabilities = await Availability.find({});
 
   if (!availabilities) {
-      res.status(404).send("No availabilities found");
-    }
-    res.status(200).json({ availabilities});
-})
+    res.status(404).send("No availabilities found");
+  }
+  res.status(200).json({ availabilities });
+});
