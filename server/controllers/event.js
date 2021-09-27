af8176c1fd0401c7952e65e6101a1ecda8f68bf1
mohamedelserafy <elserafy02@gmail.com>
2021-09-27 22:53:09 +0200

    updated meetup eventSchema
        - added description and address string fields
        - this should use mongodb's built in geoJSON

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -2,10 +2,9 @@ const asyncHandler = require('express-async-handler');
 const Event = require('../models/Event');
 
 exports.createEvent = asyncHandler(async (req, res, next) => {
-  const { name, eventDate, location } = req.body;
+  const { name, eventDate, location, description, address } = req.body;
   const host = req.user.id;
-  const newEvent = await Event.create({ name, eventDate, location, host });
-
+  const newEvent = await Event.create({ name, eventDate, location, description, address, host });
   return res.status(201).json(newEvent);
 });
 
--- server/models/Event.js
+++ server/models/Event.js
@@ -1,18 +1,34 @@
 const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  //Note that longitude comes first in a GeoJSON coordinate array, not latitude
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
 const eventSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     eventDate: { type: Date, required: true },
     location: {
-      latitude: { type: Number },
-      longitude: { type: Number },
-      address: { type: String },
+      type: pointSchema,
+      required: true,
+      index: '2dsphere',
     },
     attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'profile' }],
     host: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'profile',
     },
+    description: String,
+    address: String,
   },
   { timestamps: true },
 );
