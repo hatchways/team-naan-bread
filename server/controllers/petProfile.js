3989c78e32d866cf5d7cfbafd41056be7b251eb5
konradkop <konrad.kopko@baruchmail.cuny.edu>
2021-09-27 23:44:28 -0400

    formatting and auth

--- server/controllers/petProfile.js
+++ server/controllers/petProfile.js
@@ -2,62 +2,55 @@ const PetProfile = require("../models/PetProfile");
 const asyncHandler = require("express-async-handler");
 //the Profile is also created on in controllers/auth when the user creates a new account. However- only the email and id fields will be filled
 
-exports.createPetProfile= asyncHandler(async (req, res, next) => {
+exports.createPetProfile = asyncHandler(async (req, res, next) => {
+  const { petName, ownerID, petType, petAge, petStatus } = req.body;
 
-    const { 
-        petName,
-        ownerID,
-        petType,
-        petAge,
-        petStatus,
-     } = req.body;
+  const petProfile = await PetProfile.create({
+    petName,
+    ownerID,
+    petType,
+    petAge,
+    petStatus,
+  });
 
-    const petProfile = await PetProfile.create({
-        petName,
-        ownerID,
-        petType,
-        petAge,
-        petStatus,
-      })
-
-      res.status(200).json({petProfile});
-})
+  res.status(200).json({ petProfile });
+});
 
 exports.updatePetProfile = asyncHandler(async (req, res, next) => {
-    const values = {
-        _id,
-        petName,
-        ownerID,
-        petType,
-        petAge,
-        petStatus,
-     } = req.body;
+  const values = ({ _id, petName, ownerID, petType, petAge, petStatus } =
+    req.body);
 
-     for(let prop in values) if(!values[prop]) delete values[prop];//it will remove fields who are undefined or null
+  for (let prop in values) {
+    if (!values[prop]) {
+      delete values[prop];  //it will remove fields who are undefined or null
+    }
+  }
 
-     const petProfile = await PetProfile.findOneAndUpdate({_id: values._id}, values, {new: true, strict: false});
+  const petProfile = await PetProfile.findOneAndUpdate(
+    { _id: values._id },
+    values,
+    { new: true, strict: false }
+  );
 
-     res.status(200).json({petProfile});
-
-})
+  res.status(200).json({ petProfile });
+});
 exports.findPetProfilesByOwnerId = asyncHandler(async (req, res, next) => {
+  const petProfile = await petProfile.findById({ ownerID: req.body.ownerID });
+  if (!petProfile) {
+    res.status(404);
+    throw new Error("No pet profile found for ID");
+  }
 
-     const petProfile = await petProfile.findById({ownerID: req.body.ownerID});
-     if (!petProfile) {
-        res.status(404);
-        throw new Error("No pet profile found for ID");
-      }
-    
-      res.status(200).json(petProfile);
-})
+  res.status(200).json(petProfile);
+});
 
 exports.findAllPetProfiles = asyncHandler(async (req, res, next) => {
-    const petProfiles = await PetProfile.find({});
+  const petProfiles = await PetProfile.find({});
 
-    if (!petProfiles) {
-        res.status(404);
-        throw new Error("No profiles found");
-      }
+  if (!petProfiles) {
+    res.status(404);
+    throw new Error("No profiles found");
+  }
 
-    res.status(200).json({ petProfiles });
-})
+  res.status(200).json({ petProfiles });
+});
--- server/routes/petProfile.js
+++ server/routes/petProfile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const protect = require("../middleware/auth");
 const router = express.Router();
 const {
 createPetProfile,
@@ -7,12 +8,12 @@ findPetProfilesByOwnerId,
 findAllPetProfiles,
 } = require('../controllers/petProfile');
 
-router.route('/createPetProfile').post(createPetProfile);
+router.route('/createPetProfile').post(protect, createPetProfile);
 
-router.route('/updatePetProfile').post(updatePetProfile);
+router.route('/updatePetProfile').post(protect, updatePetProfile);
 
-router.route('/findPetProfilesByOwnerId').post(findPetProfilesByOwnerId);
+router.route('/findPetProfilesByOwnerId').post(protect, findPetProfilesByOwnerId);
 
-router.route('/findAllPetProfiles').get(findAllPetProfiles);
+router.route('/findAllPetProfiles').get(protect, findAllPetProfiles);
 
 module.exports = router;
