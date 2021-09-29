3f8a1d9af448ca7e85573fd71ac8583915427ddb
mohamedelserafy <elserafy02@gmail.com>
2021-09-29 14:31:04 +0200

    FE: update PetEventForm.tsx
        - fetches one event by id if exists and adds its values to form

--- client/src/components/PetEvent/PetEventForm.tsx
+++ client/src/components/PetEvent/PetEventForm.tsx
@@ -1,20 +1,47 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { LoadScript, StandaloneSearchBox } from '@react-google-maps/api';
 import { Box, Button, Paper, TextField, Typography, CircularProgress } from '@material-ui/core';
 import { DateTimePicker } from '@material-ui/pickers';
 
-import { createPetEvent } from '../../helpers/APICalls/petEvent';
+import { createPetEvent, editEvent, getOneSimpleEventById } from '../../helpers/APICalls/petEvent';
 import { useSnackBar } from '../../context/useSnackbarContext';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { PetEvent } from '../../interface/PetEvent';
+import { useAuth } from '../../context/useAuthContext';
+
+interface urlParams {
+  eventID: string;
+}
 
 export default function PetEventForm(): JSX.Element {
   const [search, setSearch] = useState<google.maps.places.SearchBox>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoadingEvent, setIsLoadingEvent] = useState<boolean>(true);
   const [newPetMeetupEvent, setNewPetMeetupEvent] = useState<PetEvent>({} as PetEvent);
 
   const { updateSnackBarMessage } = useSnackBar();
   const history = useHistory();
+  const { eventID } = useParams<urlParams>();
+  const { loggedInUser } = useAuth();
+
+  useEffect(() => {
+    async function fetchEvent() {
+      const response = await getOneSimpleEventById(eventID);
+      if (response.ok) {
+        const data = await response.json();
+        setNewPetMeetupEvent(data);
+        setIsLoadingEvent(false);
+
+        if (loggedInUser && data.host !== loggedInUser?.id) {
+          history.push('/events');
+        }
+      }
+    }
+    if (eventID) {
+      fetchEvent();
+    }
+    setIsLoadingEvent(false);
+  }, [loggedInUser, history, eventID]);
 
   const onPlacesChanged = () => {
     const newPlace = search?.getPlaces();
@@ -35,22 +62,47 @@ export default function PetEventForm(): JSX.Element {
 
   const submitPetEventForm = async (event: ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsLoading(true);
 
     if (newPetMeetupEvent) {
-      setIsLoading(true);
-      const response = await createPetEvent(newPetMeetupEvent);
-      setIsLoading(false);
+      let response;
+      if (eventID) {
+        response = await editEvent(eventID, newPetMeetupEvent);
+      } else {
+        response = await createPetEvent(newPetMeetupEvent);
+      }
       if (response.ok) {
         updateSnackBarMessage('event created');
         const newPetEvent = await response.json();
-        console.log(newPetEvent._id);
-
         history.push(`/event/${newPetEvent._id}`);
       } else {
         updateSnackBarMessage('something went wrong');
       }
     }
+    setIsLoading(false);
   };
+
+  if (isLoadingEvent) {
+    return (
+      <Box padding="50%">
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!loggedInUser) {
+    return (
+      <Box padding="50%">
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!newPetMeetupEvent.name && eventID) {
+    return (
+      <Box padding="50%">
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <Box padding={'10%'} alignItems="center">
@@ -63,6 +115,7 @@ export default function PetEventForm(): JSX.Element {
           <Box flexDirection="column" display="flex" alignItems="stretch" justifyContent="center" padding={'10%'}>
             <Box m={2}>
               <TextField
+                defaultValue={newPetMeetupEvent.name}
                 onChange={(event) => {
                   setNewPetMeetupEvent((state: PetEvent) => {
                     state.name = event.target.value;
@@ -71,9 +124,9 @@ export default function PetEventForm(): JSX.Element {
                 }}
                 fullWidth
                 required
-                label={'name'}
+                label={'add a name'}
                 variant="outlined"
-                type="text"
+                InputLabelProps={{ shrink: true }}
               />
             </Box>
 
@@ -93,6 +146,7 @@ export default function PetEventForm(): JSX.Element {
                       id="searchBox"
                       type="text"
                       fullWidth
+                      defaultValue={newPetMeetupEvent.address}
                     />
                   </StandaloneSearchBox>
                 </LoadScript>
@@ -128,6 +182,8 @@ export default function PetEventForm(): JSX.Element {
                 variant="outlined"
                 type="textarea"
                 label="add a description"
+                defaultValue={newPetMeetupEvent.description}
+                InputLabelProps={{ shrink: true }}
               />
             </Box>
             <Box m={3}>
